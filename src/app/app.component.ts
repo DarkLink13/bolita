50b1b7f7e1cfebf6c5962826d5e038146914dc36
Jorge Amado <darklink1302@gmail.com>
2023-04-14 21:43:41 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import * as pick3 from './pick3.json';
+import * as pick4 from './pick4.json';
+
+const COLORS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bolita';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['L', 'M', 'X', 'J', 'V', 'S', 'D'];
+  pick3a = Object(pick3)
+  pick4a = Object(pick4)
+  
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+
+  datePickerForm: FormGroup
+  mods = [{id:1,color:'red',name:'Diagonales'},{id:2,color:'blue',name:'Lunes'}, {id:3,color:'yellow',name:'Dias de la semana'}]
+  constructor(private fb: FormBuilder){
+    this.datePickerForm = this.fb.group({
+        since: new FormControl(),
+        to: new FormControl(),
+        mod: new FormControl(),
+        time: new FormControl(),
+      })
+  }
+
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLineModule, MatCommonModule, MatOptionModule, MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSelectModule } from '@angular/material/select'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    MatFormFieldModule,
+    MatLineModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatCommonModule,
+    MatIconModule,
+    MatOptionModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
